0f0eefc2ddfa3e1eedc7c8433664fcabf006d881
Joy Karmoker <joykarmoker353@gmail.com>
2021-06-20 21:34:01 +0600

    Web app using node and express

---
+++ bmiCalculator.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/", function(req, res){
+
+  var height= Number(req.body.height);
+  var weight = Number(req.body.weight);
+  var bmi = weight/(height*height);
+  res.send("Your bmi is "   + bmi);
+});
+
+app.listen("3000", function(req, res){
+  console.log("Listening");
+});
